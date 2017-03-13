48f5c040bcd7727157126c1a7b306f3b21d81622
Jennifer Mizhquiri <jenniferm4926@hstat.org>
2017-03-13 19:48:01 +0000

    hash puts fix

--- models/model.rb
+++ models/model.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'json'
 require 'pp' 
+
 # url = 'http://words.bighugelabs.com/api/2/e599e5fb8e6f9fc0ee92f7299cd1c834/word/json'
 # uri = URI(url)
 # response = Net::HTTP.get(uri)
@@ -15,7 +16,7 @@ class Output
     
     def get_syn(split_input)
         split_input.each { |word| 
-        pp syn(word)[:noun]
+        pp syn(word)["noun"]
         }
     end
     
