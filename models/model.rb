8a8a207d55e71682c3cb1c282ac0d6db2268cf8f
kevinorta <kevino6532@hstat.org>
2017-03-15 02:57:17 +0000

    maybe fixed multi word, over limit

--- models/model.rb
+++ models/model.rb
@@ -17,13 +17,18 @@ class Output
     end
     
     def get_syn(split_input)
+        split_output=[]
         split_input.each { |word| 
-        url = "http://words.bighugelabs.com/api/2/e599e5fb8e6f9fc0ee92f7299cd1c834/#{word}/json"
-        uri = URI(url)
-        response = Net::HTTP.get(uri)
-        @output =JSON.parse(response)["noun"].first.last.last
+        begin
+            url = "http://words.bighugelabs.com/api/2/e599e5fb8e6f9fc0ee92f7299cd1c834/#{word}/json"
+            uri = URI(url)
+            response = Net::HTTP.get(uri)
+            split_output << JSON.parse(response)["noun"].first.last.last
+        rescue
+            split_output << word
+        end
         }
-        
+        @output=split_output.join(" ")
         
         # @output =JSON.parse(response)["noun"].first.last.last
     end
