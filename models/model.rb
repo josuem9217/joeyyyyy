bbd14c81311300bda2969b9dd9d3fffa0de5138a
Josue <josuem9217@hstat.org>
2017-03-16 19:53:34 +0000

    i saved everything on c9

--- models/model.rb
+++ models/model.rb
@@ -1,6 +1,6 @@
-# require 'net/http'
-# require 'json'
-# require 'pp' 
+require 'net/http'
+require 'json'
+require 'pp' 
 
 # url = 'http://words.bighugelabs.com/api/2/e599e5fb8e6f9fc0ee92f7299cd1c834/word/json'
 # uri = URI(url)
